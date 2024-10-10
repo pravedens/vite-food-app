5b62b5bacbec0115a2554a2a43711f6b5081bcc4
pravedens <107057525+pravedens@users.noreply.github.com>
2024-10-10 19:33:31 +0500

    Get Data Axios

--- src/components/Category.vue
+++ src/components/Category.vue
@@ -1,54 +1,42 @@
 <template>
-    <div>
-        <div class="bg-white py-6 sm:py-8 lg:py-12">
-      <div class="max-w-screen-2xl px-4 md:px-8 mx-auto">
+  <div>
+    <div class="py-6 bg-white sm:py-8 lg:py-12">
+      <div class="px-4 mx-auto max-w-screen-2xl md:px-8">
         <div class="overflow-x-auto">
-          <div class="flex space-x-4 gap-8">
-            <a href="#" class="group block rounded-lg overflow-hidden flex-shrink-0 w-24">
-              <img src="https://via.placeholder.com/100" alt="Category"
-                class="w-full h-24 sm:h-24 object-cover rounded-lg">
+          <div class="flex gap-8 space-x-4">
+            <a
+              href="#"
+              class="flex-shrink-0 block w-24 overflow-hidden rounded-lg group"
+              v-for="(item, index) in categories"
+              :key="index"
+            >
+              <img
+                :src="item.icon"
+                class="object-cover w-full h-24 rounded-lg sm:h-24"
+              />
               <div class="mt-2 text-center">
-                <h3 class="text-lg text-gray-900 font-medium">Category 1</h3>
-              </div>
-            </a>
-            <a href="#" class="group block rounded-lg overflow-hidden flex-shrink-0 w-24">
-              <img src="https://via.placeholder.com/100" alt="Category"
-                class="w-full h-24 sm:h-24 object-cover rounded-lg">
-              <div class="mt-2 text-center">
-                <h3 class="text-lg text-gray-900 font-medium">Category 2</h3>
-              </div>
-            </a>
-            <a href="#" class="group block rounded-lg overflow-hidden flex-shrink-0 w-24">
-              <img src="https://via.placeholder.com/100" alt="Category"
-                class="w-full h-24 sm:h-24 object-cover rounded-lg">
-              <div class="mt-2 text-center">
-                <h3 class="text-lg text-gray-900 font-medium">Category 3</h3>
-              </div>
-            </a>
-            <a href="#" class="group block rounded-lg overflow-hidden flex-shrink-0 w-24">
-              <img src="https://via.placeholder.com/100" alt="Category"
-                class="w-full h-24 sm:h-24 object-cover rounded-lg">
-              <div class="mt-2 text-center">
-                <h3 class="text-lg text-gray-900 font-medium">Category 4</h3>
-              </div>
-            </a>
-            <a href="#" class="group block rounded-lg overflow-hidden flex-shrink-0 w-24">
-              <img src="https://via.placeholder.com/100" alt="Category"
-                class="w-full h-24 sm:h-24 object-cover rounded-lg">
-              <div class="mt-2 text-center">
-                <h3 class="text-lg text-gray-900 font-medium">Category 5</h3>
-              </div>
-            </a>
-            <a href="#" class="group block rounded-lg overflow-hidden flex-shrink-0 w-24">
-              <img src="https://via.placeholder.com/100" alt="Category"
-                class="w-full h-24 sm:h-24 object-cover rounded-lg">
-              <div class="mt-2 text-center">
-                <h3 class="text-lg text-gray-900 font-medium">Category 6</h3>
+                <h3 class="text-lg font-medium text-gray-900">
+                  {{ item.name }}
+                </h3>
               </div>
             </a>
           </div>
         </div>
       </div>
     </div>
-    </div>
+  </div>
 </template>
+
+<script setup>
+import { onMounted, ref } from "vue";
+import axios from "axios";
+
+const categories = ref([]);
+
+onMounted(() => {
+  axios
+    .get('v1/categories')
+    .then((resp) => categories.value = resp.data.data.data)
+    .catch((err) => console.log(err));
+})
+</script>
