e0de03e78767e86398f99dc6d78909e5cb6cb480
pravedens <107057525+pravedens@users.noreply.github.com>
2024-10-10 20:14:00 +0500

    Get Data Axios_2

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,12 +2,24 @@
     <div>
         <Category />
 
-        <ListFoods />
+        <ListFoods :dataFood="foods" />
     </div>
 </template>
 
 <script setup>
 import Category from '../components/Category.vue';
 import ListFoods from './ListFoods.vue';
+
+import { onMounted, ref } from "vue";
+import axios from "axios";
+
+const foods = ref([]);
+
+onMounted(() => {
+  axios
+    .get('v1/foods')
+    .then((resp) => foods.value = resp.data.data.data)
+    .catch((err) => console.log(err));
+})
 
 </script>
