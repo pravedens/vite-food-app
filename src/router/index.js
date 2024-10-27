786c3e2baea6ce6c8d3d417ce5d52d309e77f283
pravedens <107057525+pravedens@users.noreply.github.com>
2024-10-27 18:25:33 +0500

    Navigation Route

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createWebHistory, createRouter } from "vue-router";
 
 import Home from "../pages/Home.vue";
 import Contact from "../pages/Contact.vue";
+import { userAuthStore } from "../stores/authStore";
 
 const routes = [
   {
@@ -14,18 +15,21 @@ const routes = [
     path: "/contact",
     name: "Contact",
     component: Contact,
+    meta: { requiresAuth: true }
   },
 
   {
     path: "/login",
     name: "Login",
-    component: () => import('../pages/Auth/Login.vue')
+    component: () => import('../pages/Auth/Login.vue'),
+    meta: { requiresGuest: true }
   },
 
   {
     path: "/register",
     name: "Register",
-    component: () => import('../pages/Auth/Register.vue')
+    component: () => import('../pages/Auth/Register.vue'),
+    meta: { requiresGuest: true }
   },
 ];
 
@@ -33,5 +37,18 @@ const router = createRouter({
   history: createWebHistory(),
   routes,
 });
+
+router.beforeEach((to, from, next) => {
+  const store = userAuthStore();
+  if (to.meta.requiresAuth && !store.isLoggin) {
+    next('/login');
+  }
+  else if (to.meta.requiresGuest && store.isLoggin) {
+    next('/')
+  }
+  else {
+    next();
+  }
+})
 
 export default router;
